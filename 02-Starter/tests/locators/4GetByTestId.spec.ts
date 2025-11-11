d95fce1aa025598c391b9f39b8cb70dd41cbfd2f
danythedev01 <dazamar.dev01@gmail.com>
2025-11-10 19:11:52 -0600

    add changes

--- 02-Starter/tests/locators/4GetByTestId.spec.ts
+++ 02-Starter/tests/locators/4GetByTestId.spec.ts
@@ -1,27 +1,29 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('Get by test id - accept cookies', async ({ page }) => {
+test("Get by test id - accept cookies", async ({ page }) => {
+  await page.goto("");
+  // downside: using this goes again the direct user approach that the other locators take
+  //
+  const acceptCookiesButton = page.getByTestId("accept-cookies");
+  const declineCookiesButton = page.getByTestId("decline-cookies");
 
-    await page.goto('')
+  /* 
+    data-testid: by default, this is the attribute for searching. You can use a custom attribue by:
+        testIdAttribute: 'your-attribute'
+  */
 
-    const acceptCookiesButton = page.getByTestId('accept-cookies')
-    const declineCookiesButton = page.getByTestId('decline-cookies')
+  await acceptCookiesButton.click();
+  await expect(acceptCookiesButton).not.toBeVisible();
+  await expect(declineCookiesButton).not.toBeVisible();
+});
 
-    await acceptCookiesButton.click()
-    await expect(acceptCookiesButton).not.toBeVisible()
-    await expect(declineCookiesButton).not.toBeVisible()
+test("Get by test id - decline cookies", async ({ page }) => {
+  await page.goto("");
 
-})
+  const acceptCookiesButton = page.getByTestId("accept-cookies");
+  const declineCookiesButton = page.getByTestId("decline-cookies");
 
-test('Get by test id - decline cookies', async ({ page }) => {
-
-    await page.goto('')
-
-    const acceptCookiesButton = page.getByTestId('accept-cookies')
-    const declineCookiesButton = page.getByTestId('decline-cookies')
-
-    await declineCookiesButton.click()
-    await expect(acceptCookiesButton).not.toBeVisible()
-    await expect(declineCookiesButton).not.toBeVisible()
-
-})
+  await declineCookiesButton.click();
+  await expect(acceptCookiesButton).not.toBeVisible();
+  await expect(declineCookiesButton).not.toBeVisible();
+});
