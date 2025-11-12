753f6a3d6f1d6d74cf02419a41e4fc9f7439e6b0
danythedev01 <dazamar.dev01@gmail.com>
2025-11-12 12:03:34 -0600

    annotations

--- 02-Starter/tests/configure/1Annotations/3Only.spec.ts
+++ 02-Starter/tests/configure/1Annotations/3Only.spec.ts
@@ -1,17 +1,19 @@
-import { test, expect } from '@playwright/test'
+import { test, expect } from "@playwright/test";
 
-test('Test 1', () => {
-    console.log('test 1')
-})
+test("Test 1", () => {
+  console.log("test 1");
+});
 
-test('Test 2', () => {
-    console.log('test 2')
-})
+// using "only" will make playwright only to run this test - it refers to the whole testing suite, not only this single file
+// test.only("Test 2", () => {
+test("Test 2", () => {
+  console.log("test 2");
+});
 
-test('Test 3', () => {
-    console.log('test 3')
-})
+test("Test 3", () => {
+  console.log("test 3");
+});
 
-test('Test 4', () => {
-    console.log('test 4')
-})
+test("Test 4", () => {
+  console.log("test 4");
+});
